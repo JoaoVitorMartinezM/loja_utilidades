8a92f973045e65606cb311bfe310ef39b8bc5ec7
Joao Vitor Martinez <justdatascientist@gmail.com>
2022-11-13 12:20:36 -0300

    Problema para editar uma venda

--- src/main/java/com/loja/utilidades/services/VendaService.java
+++ src/main/java/com/loja/utilidades/services/VendaService.java
@@ -54,42 +54,42 @@ public class VendaService {
         return repositorio.save(venda);
     }
 
-//    public Venda edit(Venda venda, Integer id){
-//        if (repositorio.existsById(id)){
-//
-//            Double vendaValorTotal = 0.0;
-//            Double vendaQuantidade = 0.0;
-//            Venda vendaEditada = repositorio.findById(id).get();
-//            Cliente cliente = clienteRepo.findById(1).get();
-//            List<VendaItem> listaItens = new ArrayList<>();
-//
-//            for (VendaItem item: venda.getProdutos()) {
-//                item.setVenda(venda);
-//                Produto produto = produtoRepo.findById(item.getProduto().getId()).get();
-//                item.setProduto(produto);
-//                item.setValorUnitario(produto.getValorUnitario());
-//                item.setValorTotal(produto.getValorUnitario() * item.getQuantidade());
-//
-//                vendaValorTotal = item.getValorTotal();
-//                vendaQuantidade = item.getQuantidade();
-//                listaItens.add(item);
-//
-//            }
-//
-//
-//            vendaEditada.setProdutos(listaItens);
-//            vendaEditada.setQuantidade(vendaQuantidade);
-//            vendaEditada.setValor(vendaValorTotal);
-//            vendaEditada.setCliente(cliente);
-//
-//
-//
-//
-//            return repositorio.save(vendaEditada);
-//
-//        }
-//        return null;
-//    }
+    public Venda edit(Venda venda, Integer id){
+        if (repositorio.existsById(id)){
+
+            Double vendaValorTotal = 0.0;
+            Double vendaQuantidade = 0.0;
+            Venda vendaEditada = repositorio.findById(id).get();
+            Cliente cliente = clienteRepo.findById(1).get();
+            List<VendaItem> listaItens = new ArrayList<>();
+
+            for (VendaItem item: venda.getProdutos()) {
+                item.setVenda(venda);
+                Produto produto = produtoRepo.findById(item.getProduto().getId()).get();
+                item.setProduto(produto);
+                item.setValorUnitario(produto.getValorUnitario());
+                item.setValorTotal(produto.getValorUnitario() * item.getQuantidade());
+
+                vendaValorTotal = item.getValorTotal();
+                vendaQuantidade = item.getQuantidade();
+                listaItens.add(item);
+
+            }
+
+
+            vendaEditada.setProdutos(listaItens);
+            vendaEditada.setQuantidade(vendaQuantidade);
+            vendaEditada.setValor(vendaValorTotal);
+            vendaEditada.setCliente(cliente);
+
+
+
+
+            return repositorio.save(vendaEditada);
+
+        }
+        return null;
+    }
 
     public Boolean delete(Integer id){
         if (repositorio.existsById(id)){
